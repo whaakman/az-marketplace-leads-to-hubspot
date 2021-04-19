fd562a914cbfceb0d84aede30cffda6399edf4d2
Wesley Haakman <whaakman@live.com>
2021-04-19 19:55:29 +0200

    Added config builder (Microsoft.Extension.Configuration)

--- MarketPlaceToHubSpot.cs
+++ MarketPlaceToHubSpot.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration; 
 
 namespace evalan_hubspot
 {
@@ -15,9 +16,17 @@ namespace evalan_hubspot
         [FunctionName("MarketPlaceToHubSpot")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ExecutionContext context,
             ILogger log)
         {
-            log.LogInformation("Processing lead");
+            	  var config = new ConfigurationBuilder()
+	    .SetBasePath(context.FunctionAppDirectory)
+	    .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+	    .AddEnvironmentVariables() 
+	    .Build();
+				
+	    //string hubspotAPIKey = config["hubspotAPIKEY"];
+        log.LogInformation("Processing lead");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
